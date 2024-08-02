1ab14ee5962eacd05fa9e6c9c6dba4a9c18c700f
chef-nic <nicholasajohnson@me.com>
2024-08-02 16:31:34 -0700

    Initial Commit

---
+++ Rolodex/RolodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RolodexApp.swift
+//  Rolodex
+//
+//  Created by Nicholas Johnson on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RolodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
